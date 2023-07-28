067caacef67311238f6b4e628b8fc4c33c7c7a3d
autonitin <np8121015@gmail.com>
2023-07-28 09:38:33 +0530

    t3 and t4

--- src/test/java/demo/RestApiTesting/DemoTest.java
+++ src/test/java/demo/RestApiTesting/DemoTest.java
@@ -13,4 +13,14 @@ public class DemoTest {
 	public void t2() {
 		System.out.println("nitin test by hodu");
 	}
+	
+	@Test
+	public void t3() {
+		System.out.println("DemoTest.t3()");
+	}
+	
+	@Test
+	public void t4() {
+		System.out.println("DemoTest.t4()");
+	}
 }
